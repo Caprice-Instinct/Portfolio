0aedc5ee1d9db093611979ab759b8a2117ddced1
Caprice-Instinct <linetwangui04@gmail.com>
2024-10-24 22:40:01 +0300

    requirements.txt, render app titles in columns, import panda

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
@@ -13,3 +14,26 @@ with col2:
     niche. I love penguins🐧 and pandas🐼. I love cakes🎂 I love watching anime
     and bojack horseman."""
     st.info(content)
+
+content2 = """
+Below are some of the Python apps I built. Feel free to contact
+me in case of any questions.
+"""
+
+st.write(content2)
+
+col3, col4 = st.columns(2)
+
+# Read data.csv using pandas
+data = pandas.read_csv("data.csv", sep=";")
+
+# Display the apps in two colums
+# First column
+with col3:
+    for index, row in data[:10].iterrows():
+        st.header(row['title'])
+
+# Second column
+with col4:
+    for index, row in data[10:].iterrows():
+        st.header(row['title'])
