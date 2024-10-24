fa2a30aeb8e6689bbe405dd0938ab889d041665f
Caprice-Instinct <linetwangui04@gmail.com>
2024-10-24 22:50:04 +0300

    add description, image and links

--- main.py
+++ main.py
@@ -2,18 +2,22 @@ import streamlit as st
 import pandas
 
 st.set_page_config(layout="wide")
+
+# Create two columns
 col1, col2 = st.columns(2)
 
+# First column with my image
 with col1:
     st.image("images/photo.png")
 
+# Second column with my description
 with col2:
     st.title("Linet Wangui")
-    content = """
+    description = """
     Hi. My name is Linet. I am a programmer trying to everything to figure my
     niche. I love penguins🐧 and pandas🐼. I love cakes🎂 I love watching anime
     and bojack horseman."""
-    st.info(content)
+    st.info(description)
 
 content2 = """
 Below are some of the Python apps I built. Feel free to contact
@@ -22,18 +26,29 @@ me in case of any questions.
 
 st.write(content2)
 
-col3, col4 = st.columns(2)
+# Pass a list of the ratio dimensions of the columns
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 # Read data.csv using pandas
 data = pandas.read_csv("data.csv", sep=";")
 
-# Display the apps in two colums
+# Display the apps in two columns
 # First column
 with col3:
+    # Specify the rows
     for index, row in data[:10].iterrows():
+        # Access the data using the keys
         st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/" + row['image'])
+        st.write(f"[Source code]({row['url']})")
+
 
 # Second column
 with col4:
     for index, row in data[10:].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/" + row['image'])
+        st.write(f"[Source code]({row['url']})")
+
