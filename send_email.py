6cd37b42bdaf5fb1d90107bb23f2fad7ba0156bf
Caprice-Instinct <linetwangui04@gmail.com>
2024-10-27 17:58:50 +0300

    Secure email

--- send_email.py
+++ send_email.py
@@ -1,5 +1,6 @@
 import smtplib
 import ssl
+import os
 
 
 def send_email(message):
@@ -7,7 +8,7 @@ def send_email(message):
     port = 465
 
     username = "linetwangui04@gmail.com"
-    password = "xygyhivxitezzzps"
+    password = os.getenv("PASSWORD")
 
     receiver = "linetwangui04@gmail.com"
     context = ssl.create_default_context()
